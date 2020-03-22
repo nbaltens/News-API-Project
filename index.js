ca9d720eff9084ef36ec70aa42699f13f651e50c
Noah Baltensperger <nbalten@gmail.com>
2020-03-22 15:29:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-$.ajax({
-    url: 'http://newsapi.org/v2/top-headlines?country=us&apiKey=81ed012f2e554004a6ca2ebdf0df181d',
-    dataType: 'json',
-    success: function(data) {
-      let users = data.articles;
+// LOAD WHATS TRENDING ON PAGE LOAD
+topHeadlines();
 
-
-let articles = `<ul>`
-
-$.each(users, (user) => {
-    articles += `<li>
-            <div class="articleContainer">
-
-                <div class="articleContent">
-                    <p class="articleSrc"><i>${users[user].source.name}</i></p>
-                    <p class="articleTitle"><i>${users[user].title}</i></p>
-                    <p class="articleDesc"><i>${users[user].description}</i></p>
-                </div>
-                <div>
-                    <img class="newsImg" src="${users[user].urlToImage}">
-                    <p class="articleLink"><i><a target="_blank" href="${users[user].url}">Read More</a></i></p>
-
-                </div>
-                
-            </div>
-                </li>`
-});
-
-articles += `</ul>`;
-$('#article').html(articles);
-
-
-$('#searchbox').on('keypress',function(e) {
-    if(e.which == 13) {
-        alert('You pressed enter!');
-    }
-});
-
-
-
-    }
+// GO BACK TO TRENDING WHEN LOGO CLICKED
+$("#homeLogo").click(() => {
+    topHeadlines()
   });
 
+
+// SWITCH URL TO SEARCH INPUT
+$('#searchbox').on('keypress',function(e) {
+    let searchInput = $('#searchbox').val();
+    if(e.which == 13) {
+        getSearch(searchInput);
+        $("#searchbox").val("");
+    } else {
+        return
+    }
+    
+});
+
+// CREATE API CALL FOR TOP-HEADLINES
+function topHeadlines() {
+    $.ajax({
+        url: `http://newsapi.org/v2/top-headlines?country=us&apiKey=81ed012f2e554004a6ca2ebdf0df181d`,
+        dataType: 'json',
+        success: function(data) {
+        let articleData = data.articles;
+        makeArticle(articleData);
+        }
+      });
+}
+
+// CREATE API CALL FOR SEARCH RESULTS
+ function getSearch(event) {
+    $.ajax({
+        url: `http://newsapi.org/v2/everything?q=${event}&apiKey=81ed012f2e554004a6ca2ebdf0df181d`,
+        dataType: 'json',
+        success: function(data) {
+        let articleData = data.articles;
+        makeArticle(articleData);
+        }
+      });
+ }
+
  
+ 
+//  CREATE ARTICLES
+ function makeArticle(articleData) {
+    let articles = `<ul>`
+    $.each(articleData, (data) => {
+        articles += `<li>
+                <div class="articleContainer">
+                    <div class="articleContent">
+                        <p class="articleSrc"><i>${articleData[data].source.name}</i></p>
+                        <p class="articleTitle"><i>${articleData[data].title}</i></p>
+                        <p class="articleDesc"><i>${articleData[data].description}</i></p>
+                    </div>
+                    <div>
+                        <img class="newsImg" src="${articleData[data].urlToImage}">
+                        <p class="articleLink"><i><a target="_blank" href="${articleData[data].url}">Read More</a></i></p>
+                    </div> 
+                </div>
+                    </li>`
+    });
+    
+    articles += `</ul>`;
+    $('#article').html(articles);
+ }
